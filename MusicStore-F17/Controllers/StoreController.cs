9dfb1533ac9bf918ae5df206c77356f4ea66b2b7
Mathias <mathias__vp@hotmail.com>
2017-09-27 13:33:51 -0400

    Introduced db connection, Browse

--- MusicStore-F17/Controllers/StoreController.cs
+++ MusicStore-F17/Controllers/StoreController.cs
@@ -9,27 +9,39 @@ namespace MusicStore_F17.Controllers
 {
     public class StoreController : Controller
     {
+        //add db connection
+        MusicStoreModel db = new MusicStoreModel();
+
         // GET: Store
         public ActionResult Index()
         {
             //mock up some album data and pass that to the view
-            var genres = new List<Genre>();
+            //var genres = new List<Genre>();
 
-            for(int i=0;i <= 10; i++)
-            {
-                genres.Add(new Genre { Name = "Genre " + i });
-            }
-            ViewBag.genres = genres;
+            //for(int i=0;i <= 10; i++)
+            //{
+            //    genres.Add(new Genre { Name = "Genre " + i });
+            //}
+            //ViewBag.genres = genres;
+
+            //use the connection and Genre model to query the genre table
+            var genres = from g in db.Genres
+                         orderby g.Name ascending
+                         select g;
+
             ViewBag.Message = "Please select a genre";
-            return View();
+            return View(genres);
         }
 
         // GET: Store/Browse
-        public ActionResult Browse(string Genre)
+        public ActionResult Browse(string genre)
         {
+            //use our Album model to display the related albums
+            var selectedGenre = db.Genres.Include("Albums").Single(g => g.Name == genre);
+            
             // Send genre back to the View
-            ViewBag.Genre = Genre;
-            return View();
+            ViewBag.Genre = genre;
+            return View(selectedGenre);
         }
     }
 }
